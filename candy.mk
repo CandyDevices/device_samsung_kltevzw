f589b4e6437571d822cf483ccf2f8f57a25525bc
gimmeitorilltell <gimmeitorilltell@yahoo.com>
2017-02-20 20:32:59 -0500

    Candify kltevzw

---
+++ candy.mk
@@ -0,0 +1,6 @@
+$(call inherit-product, vendor/candy/config/common_full_phone.mk)
+
+$(call inherit-product, device/samsung/kltevzw/full_kltevzw.mk)
+
+PRODUCT_DEVICE := kltevzw
+PRODUCT_NAME := candy_kltevzw
